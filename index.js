9af5061644fe4864c677f45ba856362a3e35c5ed
halezmarie <teacherhaleymarie@gmail.com>
2021-08-11 14:43:40 -0500

    commit tree aa3db5596a19caeee3c7b841d68e1cfa11a4c3e4

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Biscotto';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Ace';
+}
+
+// cant change a const so it stays the same name even though we tried to do a function to change it 
